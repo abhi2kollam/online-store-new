a77f7e7863700fc874a2148b22827e6f90bec571
Abhilash Pillai <abhilash.pillai@hrblock.com>
2025-11-28 17:08:34 +0530

    build fix

--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -74,7 +74,7 @@ export default function ProfilePage() {
                         products (name)
                     )
                 `)
-                .eq('user_id', user.id)
+                .eq('user_id', user?.id)
                 .order('created_at', { ascending: false });
 
             if (ordersData) {
